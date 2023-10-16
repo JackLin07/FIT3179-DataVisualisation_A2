70bea9ddb193a227a120613f77c862de8c452ecb
JackLin07 <linkaijie07@gmail.com>
2023-10-17 06:24:01 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,13 @@ var vg_1 = "test.vg.json";
 vegaEmbed("#Interest_rate_trend", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var map_vg = "globe_map.vg.json";
+vegaEmbed("#Map_of_Interest_Rates", map_vg).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var map_vg1 = "map.vg.json";
+vegaEmbed("#Map_of_everything", map_vg1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
