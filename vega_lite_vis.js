54d08f540c3c36624f29362937388bc4014937d8
JackLin07 <linkaijie07@gmail.com>
2023-10-17 06:57:14 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,14 +1,14 @@
-var vg_1 = "test.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/JackLin07/FIT3179-DataVisualisation_A2/main/codes/test.vg.json";
 vegaEmbed("#Interest_rate_trend", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var map_vg = "globe_map.vg.json";
+var map_vg = "https://raw.githubusercontent.com/JackLin07/FIT3179-DataVisualisation_A2/main/codes/globe_map.vg.json";
 vegaEmbed("#Map_of_Interest_Rates", map_vg).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var map_vg1 = "map.vg.json";
+var map_vg1 = "https://raw.githubusercontent.com/JackLin07/FIT3179-DataVisualisation_A2/main/codes/map.vg.json";
 vegaEmbed("#Map_of_everything", map_vg1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
